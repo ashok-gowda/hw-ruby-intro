c7acd72458e728742f4babc7e396ef79a3bdbe19
Ashok Govinda Gowda <8asokagowda@gmail.com>
2017-09-12 18:44:49 -0500

    Sum arr and max_2_sum functionality commits
    This commit is for the code of sum arr and max_2_sum functionality

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -2,16 +2,42 @@
 
 # Part 1
 
-def sum arr
-  # YOUR CODE HERE
+def sum(arr)
+  sum=0
+  arr.each do |i|
+  	sum=sum+i
+  end
+  return sum
 end
 
-def max_2_sum arr
-  # YOUR CODE HERE
+def max_2_sum(arr)
+  sum=0
+  if arr.length!=0
+     if arr.length==1
+       sum=sum+arr[0]
+     else
+      sum=sum+arr.max
+      first_max_index=arr.index(arr.max)
+      arr.delete_at(first_max_index)
+      sum=sum+arr.max
+     end
+  end
+return sum
 end
 
-def sum_to_n? arr, n
-  # YOUR CODE HERE
+def sum_to_n?(arr=[1,2,3,4,5],n=5)
+  if arr.length==0 || arr.length==1
+     return false
+  else
+    for i in (0..arr.length)
+      for j in (i+1..arr.length)
+        if arr[i]+arr[j]==n
+          return true
+        end
+      end
+    end
+    return false
+  end
 end
 
 # Part 2
